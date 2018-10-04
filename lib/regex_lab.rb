5ddd0246eccd7b68cc182f70f29914f997cd2b7a
Ray Valdez <ray_valdez@hotmail.co.uk>
2018-10-04 15:06:50 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,5 +1,9 @@
 def starts_with_a_vowel?(word)
-  !!(word =~ /\A[aeiou]/)
+  if word.match(/\A[aeiouAEIOU]/)
+    true
+  else
+    false
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
