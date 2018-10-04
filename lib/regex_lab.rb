31e9ac5937999684a24febc0de535f43a9ebb3a1
Ray Valdez <ray_valdez@hotmail.co.uk>
2018-10-04 15:29:38 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -23,5 +23,8 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  !!phone.scan(/\d/).length == 10
+  if phone.scan(/\d/).length == 10
+    true
+  elsefalse
+end
 end
